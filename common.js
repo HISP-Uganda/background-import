6e9ab22ae5cb4321144ebb219b86b111f25b1ca8
Charles Olupot <olupotcharles@gmail.com>
2022-08-15 10:26:36 +0300

    Corrected bugs and added periodic import

--- common.js
+++ common.js
@@ -4,259 +4,339 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const FormData = require("form-data");
 const dotenv = require("dotenv");
+const {default: Axios} = require("axios");
 
 const result = dotenv.config();
 
 if (result.error) {
-  throw result.error;
+	throw result.error;
 }
 module.exports.getDHIS2Url1 = (uri) => {
-  if (uri !== "") {
-    try {
-      const url = new URL(uri);
-      const dataURL = url.pathname.split("/");
-      const apiIndex = dataURL.indexOf("api");
+	if (uri !== "") {
+		try {
+			const url = new URL(uri);
+			const dataURL = url.pathname.split("/");
+			const apiIndex = dataURL.indexOf("api");
 
-      if (apiIndex !== -1) {
-        return url.href;
-      } else {
-        if (dataURL[dataURL.length - 1] === "") {
-          return url.href + "api";
-        } else {
-          return url.href + "/api";
-        }
-      }
-    } catch (e) {
-      console.log(e.message);
-    }
-  }
-  return null;
+			if (apiIndex !== -1) {
+				return url.href;
+			} else {
+				if (dataURL[dataURL.length - 1] === "") {
+					return url.href + "api";
+				} else {
+					return url.href + "/api";
+				}
+			}
+		} catch (e) {
+			console.log(e.message);
+		}
+	}
+	return null;
 };
 
 module.exports.createDHIS2Auth = () => {
-  const username = process.env.DHIS2_USER;
-  const password = process.env.DHIS2_PASS;
-  return { username, password };
+	const username = process.env.DHIS2_USER;
+	const password = process.env.DHIS2_PASS;
+	return {username, password};
 };
 module.exports.createDHIS2Auth2 = () => {
-  const username = process.env.DHIS2_USER_HMIS;
-  const password = process.env.DHIS2_PASS_HMIS;
-  return { username, password };
+	const username = process.env.DHIS2_USER_HMIS;
+	const password = process.env.DHIS2_PASS_HMIS;
+	return {username, password};
 };
 
 module.exports.getDHIS2Url = () => {
-  const uri = process.env.DHIS2_URL;
-  return this.getDHIS2Url1(uri);
+	const uri = process.env.DHIS2_URL;
+	return this.getDHIS2Url1(uri);
 };
 
 module.exports.getDHIS2Url2 = () => {
-  const uri = process.env.DHIS2_URL_HMIS;
-  return this.getDHIS2Url1(uri);
+	const uri = process.env.DHIS2_URL_HMIS;
+	return this.getDHIS2Url1(uri);
 };
 
 module.exports.queryDHIS2 = async (path, params) => {
-  try {
-    const baseUrl = this.getDHIS2Url();
-    if (baseUrl) {
-      const urlx = `${baseUrl}/${path}`;
-      const { data } = await axios.get(urlx, {
-        auth: this.createDHIS2Auth(),
-        params,
-      });
-      return data;
-    }
-  } catch (e) {
-    console.log(e.message);
-  }
+	try {
+		const baseUrl = this.getDHIS2Url();
+		if (baseUrl) {
+			const urlx = `${baseUrl}/${path}`;
+			const {data} = await axios.get(urlx, {
+				auth: this.createDHIS2Auth(),
+				params,
+			});
+			return data;
+		}
+	} catch (e) {
+		console.log(e.message);
+	}
 };
 
 module.exports.query2DHIS2 = async (path, params) => {
-  // try {
-  const baseUrl = this.getDHIS2Url2();
-  if (baseUrl) {
-    const urlx = `${baseUrl}/${path}`;
-    const { data } = await axios.get(urlx, {
-      auth: this.createDHIS2Auth2(),
-      params,
-    });
-    return data;
-  }
-  // } catch (e) {
-  //   console.log(e.message);
-  // }
+	try {
+		const baseUrl = this.getDHIS2Url2();
+		if (baseUrl) {
+			const urlx = `${baseUrl}/${path}`;
+			const {data} = await axios.get(urlx, {
+				auth: this.createDHIS2Auth2(),
+				params,
+			});
+			return data;
+		}
+	} catch (e) {
+		console.log(e.message);
+	}
+};
+
+module.exports.downloadData = async (
+	remoteDataSet,
+	orgUnit,
+	period
+) => {
+	const baseUrl = this.getDHIS2Url2();
+	let remoteUrl = `${baseUrl}/dataValueSets.csv`;
+	const response = await Axios({
+		url: remoteUrl,
+		method: "GET",
+		responseType: "stream",
+		auth: this.createDHIS2Auth2(),
+		params: {
+			dataSet: remoteDataSet,
+			period,
+			orgUnit,
+			children: true,
+		},
+	});
+	response.data.pipe(fs.createWriteStream(`${remoteDataSet}.csv`));
+	return new Promise((resolve, reject) => {
+		response.data.on("end", () => {
+			resolve();
+		});
+		response.data.on("error", () => {
+			reject();
+		});
+	});
+}
+
+module.exports.processFile = async (fileName) => {
+	const dataValues = [];
+	return new Promise((resolve, reject) => {
+		const stream = fs.createReadStream(`${fileName}.csv`);
+		const parser = csv();
+		stream.on("ready", () => {
+			stream.pipe(parser);
+		});
+		parser.on("readable", function () {
+			let row;
+			while ((row = parser.read())) {
+				const {
+					dataelement: dataElement,
+					period,
+					orgunit: orgUnit,
+					categoryoptioncombo: categoryOptionCombo,
+					attributeoptioncombo: attributeOptionCombo,
+					value,
+					storedby: storedBy,
+					lastupdated: lastUpdatedBy,
+					comment,
+					followup
+				} = row
+				dataValues.push({
+					dataElement,
+					period,
+					orgUnit,
+					categoryOptionCombo,
+					attributeOptionCombo,
+					value,
+					storedBy,
+					lastUpdatedBy,
+					comment,
+					followup
+				});
+			}
+		});
+
+		parser.on("error", function (err) {
+			console.error(err.message);
+			reject();
+		});
+
+		parser.on("end", function () {
+			resolve(dataValues);
+		});
+	});
 };
 
 module.exports.postDHIS2 = async (path, postData, params) => {
-  try {
-    const baseUrl = this.getDHIS2Url();
-    if (baseUrl) {
-      const urlx = `${baseUrl}/${path}`;
-      const { data } = await axios.post(urlx, postData, {
-        auth: this.createDHIS2Auth(),
-        params,
-      });
-      return data;
-    }
-  } catch (e) {
-    console.log(e.message);
-  }
+	try {
+		const baseUrl = this.getDHIS2Url();
+		if (baseUrl) {
+			const urlx = `${baseUrl}/${path}`;
+			const {data} = await axios.post(urlx, postData, {
+				auth: this.createDHIS2Auth(),
+				params,
+			});
+			return data;
+		}
+	} catch (e) {
+		console.log(e.message);
+	}
 };
 
 module.exports.post2DHIS2 = async (path, postData, params) => {
-  try {
-    const baseUrl = this.getDHIS2Url2();
-    if (baseUrl) {
-      const urlx = `${baseUrl}/${path}`;
-      const { data } = await axios.post(urlx, postData, {
-        auth: this.createDHIS2Auth2(),
-        params,
-      });
-      return data;
-    }
-  } catch (e) {
-    console.log(e.message);
-  }
+	try {
+		const baseUrl = this.getDHIS2Url2();
+		if (baseUrl) {
+			const urlx = `${baseUrl}/${path}`;
+			const {data} = await axios.post(urlx, postData, {
+				auth: this.createDHIS2Auth2(),
+				params,
+			});
+			return data;
+		}
+	} catch (e) {
+		console.log(e.message);
+	}
 };
 
 module.exports.deleteDHIS2 = async (path) => {
-  try {
-    const baseUrl = getDHIS2Url();
-    if (baseUrl) {
-      const urlx = `${baseUrl}/${path}`;
-      const { data } = await axios.delete(urlx, {
-        auth: this.createDHIS2Auth(),
-      });
-      return data;
-    }
-  } catch (e) {
-    console.log(e.message);
-  }
+	try {
+		const baseUrl = getDHIS2Url();
+		if (baseUrl) {
+			const urlx = `${baseUrl}/${path}`;
+			const {data} = await axios.delete(urlx, {
+				auth: this.createDHIS2Auth(),
+			});
+			return data;
+		}
+	} catch (e) {
+		console.log(e.message);
+	}
 };
 
 module.exports.updateDHIS2 = async (path, postData, params) => {
-  try {
-    const baseUrl = getDHIS2Url();
-    if (baseUrl) {
-      const urlx = `${baseUrl}/${path}`;
-      const { data } = await axios.put(urlx, postData, {
-        auth: this.createDHIS2Auth(),
-        params,
-      });
-      return data;
-    }
-  } catch (e) {
-    console.log(e.message);
-  }
+	try {
+		const baseUrl = getDHIS2Url();
+		if (baseUrl) {
+			const urlx = `${baseUrl}/${path}`;
+			const {data} = await axios.put(urlx, postData, {
+				auth: this.createDHIS2Auth(),
+				params,
+			});
+			return data;
+		}
+	} catch (e) {
+		console.log(e.message);
+	}
 };
 
 module.exports.uploadDHIS2 = async (path, file, fileName, params) => {
-  try {
-    const baseUrl = getDHIS2Url();
-    if (baseUrl) {
-      const urlx = `${baseUrl}/${path}`;
-      const form = new FormData();
-      form.append(fileName, file, `${fileName}.csv`);
-      const { data } = await axios.post(urlx, form, {
-        auth: this.createDHIS2Auth(),
-        params,
-        headers: form.getHeaders(),
-      });
-      return data;
-    }
-  } catch (e) {
-    console.log(e.message);
-  }
+	try {
+		const baseUrl = getDHIS2Url();
+		if (baseUrl) {
+			const urlx = `${baseUrl}/${path}`;
+			const form = new FormData();
+			form.append(fileName, file, `${fileName}.csv`);
+			const {data} = await axios.post(urlx, form, {
+				auth: this.createDHIS2Auth(),
+				params,
+				headers: form.getHeaders(),
+			});
+			return data;
+		}
+	} catch (e) {
+		console.log(e.message);
+	}
 };
 
 module.exports.validText = (dataType, value) => {
-  switch (dataType) {
-    case "TEXT":
-    case "LONG_TEXT":
-      return !!value;
-    case "NUMBER":
-      return !isNaN(Number(value));
-    case "EMAIL":
-      const re = /\S+@\S+\.\S+/;
-      return re.test(String(value).toLowerCase());
-    case "BOOLEAN":
-      return (
-        String(value).toLowerCase() === "false" ||
-        String(value).toLowerCase() === "true"
-      );
-    case "TRUE_ONLY":
-      return String(value).toLowerCase() === "true";
-    case "PERCENTAGE":
-      return Number(value) >= 0 && Number(value) <= 100;
-    case "INTEGER":
-      return !isNaN(Number(value)) && Number.isInteger(Number(value));
-    case "UNIT_INTERVAL":
-      return Number(value) >= 0 && Number(value) <= 1;
-    case "INTEGER_NEGATIVE":
-    case "NEGATIVE_INTEGER":
-      return Number.isInteger(Number(value)) && Number(value) < 0;
-    case "INTEGER_ZERO_OR_POSITIVE":
-    case "AGE":
-      const v = Number(value);
-      return !isNaN(v) && Number.isInteger(v) && v >= 0;
-    case "COORDINATE":
-      try {
-        const c = JSON.parse(value);
-        return _.isArray(c) && c.length === 2;
-      } catch (e) {
-        return false;
-      }
-    default:
-      return true;
-  }
+	switch (dataType) {
+		case "TEXT":
+		case "LONG_TEXT":
+			return !!value;
+		case "NUMBER":
+			return !isNaN(Number(value));
+		case "EMAIL":
+			const re = /\S+@\S+\.\S+/;
+			return re.test(String(value).toLowerCase());
+		case "BOOLEAN":
+			return (
+				String(value).toLowerCase() === "false" ||
+				String(value).toLowerCase() === "true"
+			);
+		case "TRUE_ONLY":
+			return String(value).toLowerCase() === "true";
+		case "PERCENTAGE":
+			return Number(value) >= 0 && Number(value) <= 100;
+		case "INTEGER":
+			return !isNaN(Number(value)) && Number.isInteger(Number(value));
+		case "UNIT_INTERVAL":
+			return Number(value) >= 0 && Number(value) <= 1;
+		case "INTEGER_NEGATIVE":
+		case "NEGATIVE_INTEGER":
+			return Number.isInteger(Number(value)) && Number(value) < 0;
+		case "INTEGER_ZERO_OR_POSITIVE":
+		case "AGE":
+			const v = Number(value);
+			return !isNaN(v) && Number.isInteger(v) && v >= 0;
+		case "COORDINATE":
+			try {
+				const c = JSON.parse(value);
+				return _.isArray(c) && c.length === 2;
+			} catch (e) {
+				return false;
+			}
+		default:
+			return true;
+	}
 };
 
 module.exports.validateValue = (dataType, value, optionSetValue, optionSet) => {
-  if (optionSetValue && !!value) {
-    const options = optionSet.options.map((o) => {
-      return {
-        code: o.code,
-        value: o.value,
-      };
-    });
-    const coded = options.find((o) => {
-      return (
-        String(value).toLowerCase() === String(o.code).toLowerCase() ||
-        String(value).toLowerCase() === String(o.value).toLowerCase()
-      );
-    });
-    if (!!coded) {
-      return coded.code;
-    }
-  } else if (!!value && this.validText(dataType, value)) {
-    if ((dataType === "BOOLEAN") | (dataType === "TRUE_ONLY")) {
-      return String(value).toLowerCase();
-    }
-    return value;
-  }
-  return null;
+	if (optionSetValue && !!value) {
+		const options = optionSet.options.map((o) => {
+			return {
+				code: o.code,
+				value: o.value,
+			};
+		});
+		const coded = options.find((o) => {
+			return (
+				String(value).toLowerCase() === String(o.code).toLowerCase() ||
+				String(value).toLowerCase() === String(o.value).toLowerCase()
+			);
+		});
+		if (!!coded) {
+			return coded.code;
+		}
+	} else if (!!value && this.validText(dataType, value)) {
+		if ((dataType === "BOOLEAN") | (dataType === "TRUE_ONLY")) {
+			return String(value).toLowerCase();
+		}
+		return value;
+	}
+	return null;
 };
 
 module.exports.readCSV = (fileName) => {
-  const results = [];
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(fileName)
-      .pipe(csv())
-      .on("data", (data) => results.push(data))
-      .on("end", () => {
-        resolve(results);
-      });
-  });
+	const results = [];
+	return new Promise((resolve, reject) => {
+		fs.createReadStream(fileName)
+			.pipe(csv())
+			.on("data", (data) => results.push(data))
+			.on("end", () => {
+				resolve(results);
+			});
+	});
 };
 
 module.exports.sendMail = () => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    auth: {
-      user: process.env.GMAIL_USERNAME,
-      pass: process.env.GMAIL_PASSWORD,
-    },
-  });
+	const transporter = nodemailer.createTransport({
+		host: "smtp.gmail.com",
+		port: 587,
+		auth: {
+			user: process.env.GMAIL_USERNAME,
+			pass: process.env.GMAIL_PASSWORD,
+		},
+	});
 
-  transporter.verify().then(console.log).catch(console.error);
+	transporter.verify().then(console.log).catch(console.error);
 };
--- ct.js
+++ ct.js
@@ -1,300 +1,196 @@
 const logger = require("./Logger");
-const {query2DHIS2, postDHIS2, readCSV} = require("./common");
-const {chunk} = require("lodash");
+const {postDHIS2, readCSV, downloadData, processFile} = require("./common");
+const {chunk, range} = require("lodash");
+const {default: Axios} = require("axios");
+const {eachMonthOfInterval, eachQuarterOfInterval, eachWeekOfInterval, parseISO, format} = require("date-fns")
 const log = logger("data-sets-repo");
+
+const original = [
+	"2019July",
+	"2020July",
+	"2021July",
+	"2022July",
+]
+const quarterly = (start, end) => {
+	const startDate = new Date(start);
+	const endDate = new Date(end);
+	return eachQuarterOfInterval({start: startDate, end: endDate}).map((d) => format(d, "yyyy'Q'Q"));
+}
+
+const months2 = (start, end) => {
+	const startDate = new Date(start);
+	const endDate = new Date(end);
+	return eachMonthOfInterval({start: startDate, end: endDate}).map((d) => format(d, "yyyyMM"))
+}
+
+const weeks = (start, end) => {
+	const startDate = new Date(start);
+	const endDate = new Date(end);
+	return eachWeekOfInterval({start: startDate, end: endDate}).map((d) => format(d, "yyyy'W'I"))
+}
 
 const dataSets = [
 	{
 		name: "_Estimates: Projected Population Estimates By Age",
 		id: "NLmYDbAYwNO",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 033b - Weekly Epidemiological Surveillance Report",
 		id: "C4oUitImBPK",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: weeks("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 097b - VHT/ICCM Quarterly Report",
 		id: "onFoQ4ko74y",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: quarterly("2020-01-01", "2022-12-31")
 	},
 	{
 		name: "HMIS 104 - NTDS MDA Implementation Report",
 		id: "ijQTwdoFdC3",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 105:01 - OPD Monthly Report (Attendance, Referrals, Conditions,TB, Nutrition)",
 		id: "RtEYsASU7PG",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 105:02-03 - OPD Monthly Report (MCH, FP, EID, EPI & HEPB)",
 		id: "ic1BSWhGOso",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 105:04-05 - OPD Monthly Report (HTS & SMC)",
 		id: "nGkMm2VBT4G",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 105:06-09 - OPD Monthly Report (Supplies, Outreaches & Supervision)",
 		id: "VDhwrW9DiC1",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 105:10 - OPD Monthly Report (Lab)",
 		id: "quMWqLxzcfO",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 105a - Specialized Outpatient monthly report",
 		id: "IAtaRO1bTAO",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 105C- Palliative Care Monthly Report",
 		id: "V6TqjXm5sQy",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 106a:01-02 - HIV Quarterly Report",
 		id: "dFRD2A5fdvn",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: quarterly("2020-01-01", "2022-12-31")
 	},
 	{
 		name: "HMIS 106a:03 - TB/Leprosy Quarterly Report",
 		id: "DFMoIONIalm",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: quarterly("2020-01-01", "2022-12-31")
 	},
 	{
 		name: "HMIS 106a:04 - Lab Quarterly Report",
 		id: "GwSIuQVi8b2",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: quarterly("2020-01-01", "2022-12-31")
 	},
 	{
 		name: "HMIS 107a - Subcounty Annual Population Projection Report",
 		id: "atRhxzpCbC1",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: original
 	},
 	{
 		name: "HMIS 107c - Health Facility Human Resource Inventory",
 		id: "A9MFd92O3GZ",
-		periodType: [
-			["2019-07-01", "2020-06-30"],
-			["2020-07-01", "2021-06-30"],
-			["2021-07-01", "2022-06-30"],
-		]
+		periodType: original
 	},
 	{
 		name: "HMIS 107 -  Health Unit Population and Annual Report             ",
 		id: "qwSAQ1CIkjp",
-		periodType: [
-			["2019-07-01", "2020-06-30"],
-			["2020-07-01", "2021-06-30"],
-			["2021-07-01", "2022-06-30"],
-		]
+		periodType: original
 	},
 	{
 		name: "HMIS 108a - Specialised Inpatient Monthly Report",
 		id: "AJPsbEM4KH7",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "_HMIS 108a - Specialised Inpatient Monthly Report-original",
 		id: "G8auYvRfXVi",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 108 - IPD Monthly Report",
 		id: "EBqVAQRmiPm",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "_HMIS 108 - IPD Monthly Report_Clone",
 		id: "X5XpcuXLyV2",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 110 - District Health Facility  WASH Report",
 		id: "bv6CZdughvo",
-		periodType: [
-			["2019-07-01", "2020-06-30"],
-			["2020-07-01", "2021-06-30"],
-			["2021-07-01", "2022-06-30"],
-		]
+		periodType: original
 	},
 	{
 		name: "HMIS 111 - District School WASH Summary Report",
 		id: "GcShGHoBAI0",
-		periodType: [
-			["2019-07-01", "2020-06-30"],
-			["2020-07-01", "2021-06-30"],
-			["2021-07-01", "2022-06-30"],
-		]
+		periodType: original
 	},
 	{
 		name: "HMIS 127b - Visceral Leishmaniasis Inpatient Monthly Report",
 		id: "Vw6UqgPheNN",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS 128b - Visceral Leishmaniasis Outpatient Monthly Report",
 		id: "zXyh8jaafWj",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "HMIS MAL 002 - Malaria Vector Monthly Reporting Form",
 		id: "YXQPPYkg1f2",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "LMIS: Test Kits Report",
 		id: "EyK1YaS3WyH",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "LMIS: TWOS - TB First Line Report",
 		id: "JmneeJ5WLij",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "LMIS: TWOS TB MDR Report",
 		id: "Z04NUtAIIWc",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "LMIS: WAOS  Report",
 		id: "LbRczM2QQSN",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: months2("2020-01-02", "2022-12-31")
 	},
 	{
 		name: "Targets : Estimated TB cases and TB Targets",
 		id: "urGNhbeoTMV",
-		periodType: [
-			["2020-01-01", "2020-12-31"],
-			["2021-01-01", "2021-12-31"],
-			["2022-01-01", "2022-12-31"],
-		]
+		periodType: quarterly("2020-01-01", "2022-12-31")
 	}
 ];
-const processDataSet = async (dataSet, orgUnit, startDate, endDate) => {
-	log.info("Fetching from hmis");
-	const {dataValues} = await query2DHIS2("dataValueSets.json", {
-		dataSet,
-		orgUnit,
-		children: true,
-		includeDeleted: true,
-		startDate,
-		endDate,
-	});
-	if (dataValues) {
+const processDataSet = async (dataSet, orgUnit, period) => {
+	log.info("Downloading from hmis");
+	await downloadData(dataSet, orgUnit, period);
+	const dataValues = await processFile(dataSet);
+	if (dataValues && dataValues.length > 0) {
 		log.info(`Found ${dataValues.length} records`);
 		log.info("Inserting in repo");
 		const requests = chunk(dataValues, 50000).map((dvs) =>
@@ -331,12 +227,12 @@ const insert = async () => {
 		for (const {id, name, periodType} of dataSets) {
 			for (const period of periodType) {
 				log.info(
-					`Processing dataSet ${name} for ${displayName} from ${period[0]} to ${period[1]}`
+					`Processing dataSet ${name} for ${displayName} for period ${period}`
 				);
-				await processDataSet(id, orgUnit, period[0], period[1]);
+				await processDataSet(id, orgUnit, period);
 			}
 		}
 	}
 };
 
-insert().then(() => console.log("Done"));
+insert().then(() => console.log("Done"))
